39f9132b653b133dc5d07c778d13cf7bb910af21
noieda <csrausyan@gmail.com>
2022-06-05 15:00:45 +0700

    add page titel

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,10 @@ import Project from "../components/project"
 export default function Home() {
   return (
     <div className="bg-red-50">
+      <Head>
+        <title>noieda</title>
+        <meta property="og:title" content="My page title" key="title" />
+      </Head>
       <div className="flex justify-center py-20">
         <div className='max-w-xl'>
           <About />
